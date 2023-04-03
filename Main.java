ea4285d7b8fd77458020ab9e66a6d18014124415
Anna Goncharova <anna.a.goncharova@gmail.com>
2023-04-03 20:49:54 +0000

    Add task description to HW

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
 public class Main {
 
     public static void main(String[] args){
+
+        //1. Надає значення 4 полям типу int.
+
         int a = 2;
         int b = 3;
         int c = 4;
         int d = 6;
 
-        String result = a+b > c+d ? "Yes" : "No";
 
-    System.out.println(result);    
+        //2. Сумує їх попарно.
+        //3. Виводить результат порівняння сум (true, якщо перша сума більша).
+
+        String sumAndCompare = c+d > a+b ? "Yes" : "No";
+        System.out.println(sumAndCompare);    
+        
+
+        //4. Збільшує першу суму на 1.
+        
+        
     }
 }
