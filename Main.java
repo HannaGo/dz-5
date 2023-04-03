b12e3575bffb37a7c68b4d8fbceab54d94d7d62d
Anna Goncharova <anna.a.goncharova@gmail.com>
2023-04-03 20:12:09 +0000

    commit blueprint

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        int d = 6;
+
+        String result = a+b > c+d ? "Yes" : "No";
+
+    System.out.println(result);    
+    }
+}
