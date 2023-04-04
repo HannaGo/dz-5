aa0655acc4c77291788229146dcc978ee87d8819
Anna Goncharova <anna.a.goncharova@gmail.com>
2023-04-04 20:00:28 +0000

    "Committed 6 tasks from 7"

--- Main.java
+++ Main.java
@@ -4,20 +4,46 @@ public class Main {
 
         //1. Надає значення 4 полям типу int.
 
-        int a = 2;
-        int b = 3;
-        int c = 4;
-        int d = 6;
+        int a = 5;
+        int b = 4;
+        int c = 3;
+        int d = 2;
 
 
         //2. Сумує їх попарно.
         //3. Виводить результат порівняння сум (true, якщо перша сума більша).
 
-        String sumAndCompare = c+d > a+b ? "Yes" : "No";
-        System.out.println(sumAndCompare);    
-        
+        String sumAndCompare = a+b > c+d ? "Yes" : "No";
+        System.out.println(sumAndCompare);   
+
+        // Or
+
+        System.out.println((a+b)>(c+d));
 
         //4. Збільшує першу суму на 1.
+
+        Integer firstSumPlusOne = (a+b);
+        System.out.println(++firstSumPlusOne);
+        
+        // 5. Другу суму зменшує на 2.
+
+        Integer secondSumPlusOne = (c+d);
+        System.out.println(secondSumPlusOne -2);
+
+        // 6. Виводить результат порівняння (true якщо перша сума більша).
+
+        String sumAndComparing = firstSumPlusOne > secondSumPlusOne ? "Yes" : "No";
+        System.out.println(sumAndComparing);   
+
+        // Or
+
+        System.out.println(firstSumPlusOne>secondSumPlusOne);
+        
+        // 7. Виводить true якщо хоча б одна сума кратна 2, інакше - false.
+
+        String sumAndComparing = firstSumPlusOne > secondSumPlusOne ? "Yes" : "No";
+        System.out.println(sumAndComparing);   
+
         
         
     }
